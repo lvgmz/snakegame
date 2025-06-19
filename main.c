c009e9ed9dc7dd62a1e891abdf35d94630aa314c
Leo <leovadork@gmail.com>
2025-06-18 22:18:53 -0500

    snake movement

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "raylib.h"
 
+#define SQUARE 20
+
 // Struct that makes SnakeSegment a x,y position for a snake segment
 typedef struct SnakeSegment {
     Vector2 position;
@@ -9,19 +11,43 @@ int main()  {
     const int screenWidth = 800;
     const int screenHeight = 600;
     InitWindow(screenWidth, screenHeight, "snakegame");
-    SetTargetFPS(60);
+    SetTargetFPS(10);
 
-    // Array that stores positions max length of 7 for testing
-    SnakeSegment snake[7];
+    // Array that stores positions max length of 3 for testing
+    SnakeSegment snake[3];
     int snakeLength = 3;
 
     //sets initial position for snake
     for (int i = 0; i < snakeLength; i++) {
-        snake[i].position = (Vector2){screenWidth/2 - i*20, screenHeight/2};
+        snake[i].position = (Vector2){screenWidth/2 - i*SQUARE, screenHeight/2};
     }
+
+    // vector that stores where snake will go next
+    Vector2 direction = {SQUARE, 0};
 
     while(!WindowShouldClose()) {
 
+        // inputs
+        if (IsKeyPressed(KEY_RIGHT) && direction.x == 0) {
+            direction = (Vector2) {SQUARE, 0};
+        }
+        if (IsKeyPressed(KEY_LEFT) && direction.x == 0) {
+            direction = (Vector2) {-SQUARE, 0};
+        }
+        if (IsKeyPressed(KEY_UP) && direction.y == 0) {
+            direction = (Vector2) {0, -SQUARE};
+        }
+        if (IsKeyPressed(KEY_DOWN) && direction.y == 0) {
+            direction = (Vector2) {0, SQUARE};
+        }
+
+        // move snake
+        for (int i = snakeLength -1; i > 0; i--) {
+            snake[i].position = snake[i-1].position;
+        }
+        snake[0].position.x += direction.x;
+        snake[0].position.y += direction.y;
+        
 
 
         BeginDrawing();
@@ -29,7 +55,7 @@ int main()  {
         
         // draws snake
         for(int i = 0; i < snakeLength; i++) {
-            DrawRectangleV(snake[i].position, (Vector2){20, 20}, DARKGREEN);
+            DrawRectangleV(snake[i].position, (Vector2){SQUARE, SQUARE}, DARKGREEN);
         }
 
 
