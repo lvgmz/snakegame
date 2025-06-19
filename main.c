6d3b17148865734ca2b6a8218d0c5ca308713213
Leo <leovadork@gmail.com>
2025-06-18 21:35:17 -0500

    draws snake on canvas by setting up a struct for positions and an array to store positions of each segment

--- main.c
+++ main.c
@@ -1,13 +1,38 @@
 #include "raylib.h"
 
+// Struct that makes SnakeSegment a x,y position for a snake segment
+typedef struct SnakeSegment {
+    Vector2 position;
+} SnakeSegment;
+
 int main()  {
-    InitWindow(800, 600, "snakegame");
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+    InitWindow(screenWidth, screenHeight, "snakegame");
     SetTargetFPS(60);
 
+    // Array that stores positions max length of 7 for testing
+    SnakeSegment snake[7];
+    int snakeLength = 3;
+
+    //sets initial position for snake
+    for (int i = 0; i < snakeLength; i++) {
+        snake[i].position = (Vector2){screenWidth/2 - i*20, screenHeight/2};
+    }
+
     while(!WindowShouldClose()) {
+
+
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Hello, World!", 10, 10, 20, DARKGRAY);
+        
+        // draws snake
+        for(int i = 0; i < snakeLength; i++) {
+            DrawRectangleV(snake[i].position, (Vector2){20, 20}, DARKGREEN);
+        }
+
+
         EndDrawing();
     }
     
