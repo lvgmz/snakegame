5e63ea3d0568cc65f88bdb4c4d24cbd0652ae867
Leo <leovadork@gmail.com>
2025-06-18 21:02:32 -0500

    setting up window

--- main.c
+++ main.c
@@ -1,20 +1,16 @@
 #include "raylib.h"
 
-int main(void)  {
-    const  int screenWidth = 800;
-    const int screenHeight = 600;
-
-    InitWindow(screenWidth, screenHeight, "raylib test");
-
+int main()  {
+    InitWindow(800, 600, "snakegame");
     SetTargetFPS(60);
 
-    while (!WindowShouldClose()) {
+    while(!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Hello World!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Hello, World!", 10, 10, 20, DARKGRAY);
         EndDrawing();
     }
-
+    
     CloseWindow();
     return 0;
-} 
+}
